0fc577a1dcfac5f80bedc4a9fc96ebdf2ef056a6
Matheus Lino <matheuslino86a@gmail.com>
2022-09-19 12:08:59 -0400

    Change database

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { JogadoresModule } from './jogadores/jogadores.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb://localhost:27017/smartranking?directConnection=true',
+      'mongodb://localhost:27017/sradmbackend?directConnection=true',
       {
         useUnifiedTopology: true,
       },
